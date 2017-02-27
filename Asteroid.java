9c11ad6dd636c607775bc0ba0bed63cecd5e99a9
MarcosRAmmerman <marcos.r.ammerman@gmail.com>
2017-02-26 22:01:15 -0600

    BASE CLASSES FINISHED
    pg 73 - pg 77 done

---
+++ Asteroid.java
@@ -0,0 +1,27 @@
+import java.awt.Polygon;
+import java.awt.Rectangle;
+
+public class Asteroid extends BaseVectorShape{
+  //ASTEROID SHAPE
+  private int[] astx = {-20,-13,0,20,22,20,12,2,-10,-22,-16};
+  private int[] asty = {20,23,17,20,16,-20,-22,-14,-17,-20,-5};
+  
+  //rotation speed
+  protected double rotVel;
+  public double getRotationVelocity(){return rotVel;}
+  public void setRotationalVelocity(double v){rotVel = v;}
+  
+  //bounding rectagle
+  public Rectangle getBounds(){
+    Rectangle r;
+    r = new Rectangle((int)getX() - 20, (int)getY() - 20,40,40);
+    return r
+  }
+  
+  //CONSTRUCTOR
+  Asteroid(){
+    setShape(new Polygon(astx,asty,astx.length));
+    setAlive(true);
+    setRotationVelocity(0.0);
+  }
+}
